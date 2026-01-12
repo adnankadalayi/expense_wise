78231280528e305ef51d8eb1d14b21a733c16e6d
adnank <adnankadalayi90@gmail.com>
2026-01-12 11:15:28 +0530

    used isar community

--- lib/app/services/storage_service.dart
+++ lib/app/services/storage_service.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
-import 'package:isar/isar.dart';
+import 'package:isar_community/isar.dart';
 import 'package:path_provider/path_provider.dart';
 
 class StorageService extends GetxService {
